b29164e8234b5fa57d748476e9896b8c0c53c5a9
emp74ark <emp74ark@gmail.com>
2023-09-21 16:04:35 +0200

    feat: support RN code inside native

---
+++ android/app/src/main/java/com/example/nativecrs/ReactActivity.kt
@@ -0,0 +1,40 @@
+package com.example.nativecrs
+
+import android.app.Activity
+import android.os.Bundle
+import com.facebook.react.PackageList
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.soloader.SoLoader
+
+class ReactActivity : Activity(), DefaultHardwareBackBtnHandler {
+    private lateinit var reactRootView: ReactRootView
+    private lateinit var reactInstanceManager: ReactInstanceManager
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SoLoader.init(this, false)
+        reactRootView = ReactRootView(this)
+        val packages: List<ReactPackage> = PackageList(application).packages
+        // Packages that cannot be autolinked yet can be added manually here, for example:
+        // packages.add(MyReactNativePackage())
+        // Remember to include them in `settings.gradle` and `app/build.gradle` too.
+        reactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(application)
+            .setCurrentActivity(this)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .addPackages(packages)
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build()
+        reactRootView?.startReactApplication(reactInstanceManager, "RnIntegration", null)
+        setContentView(reactRootView)
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        super.onBackPressed()
+    }
+}
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 
-const HelloWorld = () => {
+const Integration = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.hello}>Hello, World</Text>
+            <View style={styles.box}>
+                <Text style={styles.text}>React Native</Text>
+            </View>
         </View>
     );
 };
@@ -12,15 +14,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
+        alignItems: 'center'
     },
-    hello: {
-        fontSize: 20,
+    box: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        backgroundColor: '#76b6e4',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 40,
         textAlign: 'center',
         margin: 10,
     },
 });
 
 AppRegistry.registerComponent(
-    'rnIntegration',
-    () => HelloWorld,
+    'RnIntegration',
+    () => Integration,
 );
