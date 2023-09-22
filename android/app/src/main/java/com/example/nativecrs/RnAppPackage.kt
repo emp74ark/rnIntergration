e0d59310512c54db51ab905dc668f5ec055ff210
emp74ark <emp74ark@gmail.com>
2023-09-22 11:41:50 +0200

    fix: wrong ViewManager import

--- android/app/src/main/java/com/example/nativecrs/RnAppPackage.kt
+++ android/app/src/main/java/com/example/nativecrs/RnAppPackage.kt
@@ -1,19 +1,19 @@
 package com.example.nativecrs
 
 import android.view.View
-import android.view.ViewManager
+//import android.view.ViewManager
+import com.facebook.react.uimanager.ViewManager
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ReactShadowNode
 
 class RnAppPackage : ReactPackage {
-
-    override fun createViewManagers(
+        override fun createViewManagers(
         reactContext: ReactApplicationContext
-    ): MutableList<ViewManager  <View, ReactShadowNode<*>>> = mutableListOf()
+    ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
 
-    override fun createNativeModules(
+        override fun createNativeModules(
         reactContext: ReactApplicationContext
     ): MutableList<NativeModule> = listOf(ReactParam(reactContext)).toMutableList()
 }
